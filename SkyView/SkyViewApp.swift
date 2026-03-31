1497deae596346b8ea1619a564fce93d046206dd
Parvaggarawal01 <parvaggarwal130@gmail.com>
2026-04-01 01:09:35 +0530

    Initial Commit

---
+++ SkyView/SkyViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyViewApp.swift
+//  SkyView
+//
+//  Created by Parv Aggarwal on 01/04/26.
+//
+
+import SwiftUI
+
+@main
+struct SkyViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
